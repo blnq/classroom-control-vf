0b872ace8f8f6cf7bb8ddc5ce7671f5b7a1f593d
blnq <rob.wade.blnq@statefarm.com>
2016-05-24 10:59:44 -0500

    Update Lab 7.3
    can this be tested?

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
 #
-# Global objects like filebuckets and resource defaults should go in this file,
+# Global objects like filebuckets and resource defaults should go in this file,1
 # as should the default node definition. (The default node can be omitted
 # if you use the console and don't define any other nodes in site.pp. See
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
