ef03dcccf6c2ab24653bd14e847676ce109323bd
blnq <rob.wade.blnq@statefarm.com>
2016-05-24 10:09:43 -0500

    Lab 7.2.3
    adding exec to lab 76

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,15 +43,17 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "This is Excercise 7.1 ${::hostname}": }
-  }
-  
-node 'blnq.puppetlabs.vm' {
-  notify {"this is ${::fqdn}. " : }
-  file {'/etc/motd':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => "Copying solution for 7.1\n",
+
+#  file {'/etc/motd':
+ # ensure => file,
+  #owner => 'root',
+  #group => 'root',
+  #mode => '0644',
+  #content => "Copying solution for 7.1\n",
+  #}
+
+  exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr/bin:/usr/local/bin',
+    creates => '/etc/motd',
   }
 }
